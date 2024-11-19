e4b227a7d3573b59fa58f3635442edf2516b64ba
Jor137005 <128003065+JordanoPaganini@users.noreply.github.com>
2024-11-19 14:02:21 -0300

    Update settings.py

--- PI24/settings.py
+++ PI24/settings.py
@@ -71,10 +71,10 @@ WSGI_APPLICATION = 'PI24.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default= config('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
